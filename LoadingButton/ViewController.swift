ae4bd0a317a9cfafdc705962ff45aff9b25aa457
Shima <shima.jin@icloud.com>
2018-09-14 18:58:42 +0900

    Initial Commit

---
+++ LoadingButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingButton
+//
+//  Created by Jinsei Shima on 2018/09/14.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
